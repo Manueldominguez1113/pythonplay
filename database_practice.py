66517f97a9c7e1cba5eff9480de8254380c5054e
The reinforcements <Manuel.dominguez1113@gmail.com>
2025-01-11 13:06:57 -0500

    added database practice with python

---
+++ database_practice.py
@@ -0,0 +1,15 @@
+import sqlite3
+DATABASE_Path = "school.db"
+conn = sqlite3.connect(DATABASE_Path)
+cursor = conn.cursor()
+
+sql = (
+    '''
+    CREATE TABLE IF NOT EXISTS companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL    
+    )
+    '''
+)
+
+cursor.execute(sql)
